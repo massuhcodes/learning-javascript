14e9021ace21464fe5298b3ce1a0ca27622407e8
massuhcodes <massuhcodes@gmail.com>
2022-09-15 00:02:19 -0400

    Modified the code a bit and added comments

--- isogram/isogram/isogram.js
+++ isogram/isogram/isogram.js
@@ -1,15 +1,18 @@
-//
-// This is only a SKELETON file for the 'Isogram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// Determine whether a given word is an isogram
 
+/**
+ * Given a string, determine whether or not it is an isogram
+ * @param {String} test - the given string value
+ * @returns {Boolean} - whether or not the given string is an isogram
+ */
 export const isIsogram = (test) => {
     const modifiedTest = test
         .toLowerCase()
         .replaceAll("-", "")
         .replaceAll(" ", "")
         .split("");
-    return modifiedTest
-        .map((element, index) => index === modifiedTest.indexOf(element))
-        .every((element) => element);
+    // the current index must correspond to the current element as indexOf returns the first of that found element
+    return modifiedTest.every(
+        (element, index) => index === modifiedTest.indexOf(element)
+    );
 };
